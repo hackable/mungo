48f45fb6f70e25a0db074987a1b222f42c74c05b
Rohan <me@rohandey.com>
2012-05-16 01:50:06 +0530

    Added all files

--- install.sh
+++ install.sh
@@ -2,17 +2,6 @@ source config
 REPO_BASE = "$PARENT_DIR./repos/"
 SITE_BASE = "$PARENT_DIR./sites/"
 
-if [ ! -d "$REPO_BASE" ]; then
-    # Control will enter here if $REPO_BASE directory doesn't exist.
-    echo   "Creating base directory for repositories..."
-    mkdir  "$REPO_BASE"
-    echo   "Completed"
-fi
+echo "$REPO_BASE"
 
-
-if [ ! -d "$SITE_BASE" ]; then
-    # Control will enter here if $SITE_BASE doesn't exist.
-    echo   "Creating base directory for sites..."
-    mkdir  "$SITE_BASE"
-    echo   "Completed"
-fi
+echo "$SITE_BASE"
